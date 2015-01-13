14daf7c57620d8ba1644029cb3ecd67ce7256cce
Priya <Priya@alibook2.wv.cc.cmu.edu>
2015-01-12 19:18:34 -0500

    Initial Commit

---
+++ PhaseThrow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhaseThrow
+//
+//  Created by Priya on 1/12/15.
+//  Copyright (c) 2015 Priya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
